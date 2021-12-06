2ccb51aa6727c681572a59dbad3c273e54994945
奇趣保罗 <dreamer_paul@126.com>
2021-12-06 18:58:38 +0800

    Feat: Collections & Products Section
    展示自定义产品系列下四个产品的轮播模块

--- assets/theme.paul.js
+++ assets/theme.paul.js
@@ -14,5 +14,21 @@ document.addEventListener('page:loaded', function() {
         el: '.swiper-pagination',
       },
     });
-  })
+  });
+
+  // 所有的 paul-collection-and-products-section 都要有轮播
+  document.querySelectorAll(".paul-collection-and-products-section").forEach(item => {
+    let slideNames = item.querySelectorAll(".grid");
+
+    const swiper = new Swiper("." + item.classList[1] + " .swiper", {
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+        renderBullet: function (index, className) {
+          console.log('<span class="' + className + '">' + (slideNames[index].dataset.name || "No Name") + "</span>")
+          return '<span class="' + className + '">' + (slideNames[index].dataset.name || "No Name") + "</span>";
+        },
+      },
+    });
+  });
 });
